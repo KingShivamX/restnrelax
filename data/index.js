69810200e98dffc56a9722a050198c546dc5acb3
KingShivamX <shivamhippalgave@gmail.com>
2024-08-10 11:21:44 +0530

    varcel12

--- data/index.js
+++ data/index.js
@@ -3,15 +3,15 @@ const init = require("./data.js");
 const Listing = require("../models/listing.js");
 
 const main = async () => {
-    await mongoose.connect("mongodb://127.0.0.1:27017/restnrelax");
+    await mongoose.connect("<clod database will be connected>");
 }
 main()
 .then(()=>console.log("connection succesful"))
 .catch((err)=>console.log(err)); 
 
 const initData = async () => {
-    await Listing.deleteMany({});
-    init.data = init.data.map((obj) => ({...obj, owner: "66af04e0990cd4c25534945d"}))
+    // await Listing.deleteMany({});
+    init.data = init.data.map((obj) => ({...obj, owner: "<owener will be auto selected>"}))
     // adding the new owner: objectID, for all the objects in init.data
     await Listing.insertMany(init.data);
 };
